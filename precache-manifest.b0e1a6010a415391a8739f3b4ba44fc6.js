0edf0341ead2bd8c089993a46290dc512cb79cdb
Yu Ohno <kantillo0683@gmail.com>
2020-07-31 01:24:12 +0900

    Updates

---
+++ precache-manifest.b0e1a6010a415391a8739f3b4ba44fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4eec810e7486526311158e96d0e4af",
+    "url": "/weatherappreact/index.html"
+  },
+  {
+    "revision": "ebc11aff1ea511ebc084",
+    "url": "/weatherappreact/static/css/main.65ceaa5b.chunk.css"
+  },
+  {
+    "revision": "5a8e9898c8fbcf92a907",
+    "url": "/weatherappreact/static/js/2.5a9a333a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/weatherappreact/static/js/2.5a9a333a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc11aff1ea511ebc084",
+    "url": "/weatherappreact/static/js/main.ec9e747d.chunk.js"
+  },
+  {
+    "revision": "5c85170f0b4888011fb9",
+    "url": "/weatherappreact/static/js/runtime-main.b8ddbcf3.js"
+  }
+]);
